f92232228a4f50dc9b24b3590f3f466acc525cb6
cwdatlas <ascott@carroll.edu>
2024-03-28 09:27:25 -0600

    in progress html web endpoint

--- controllers/htmlEndpoints.go
+++ controllers/htmlEndpoints.go
@@ -1,25 +1,28 @@
 package controllers
 
 import (
+	"SpaceNewsWeb/models"
+	"SpaceNewsWeb/repo"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
-func DisplayIndex(c *gin.Context) {
-	message := c.Param("id")
+var data = map[string]string{}
+
+func Index(c *gin.Context) {
+	var articles []models.Article
+	repo.DB.Find(&articles)
 	c.HTML(http.StatusOK, "index.html", gin.H{
-		"title": message,
+		"articles": articles,
+		"title":    "Comet News",
 	})
 }
 
 func FrontPage(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", gin.H{
-		"title": "You made it to front page",
-	})
+	data["title"] = "You made it to front page"
+	c.HTML(http.StatusOK, "index.html", data)
 }
 
 func PostTest(c *gin.Context) {
-	c.HTML(http.StatusOK, "post.html", gin.H{
-		"title": "Posted",
-	})
+	c.HTML(http.StatusOK, "post.html", data)
 }
