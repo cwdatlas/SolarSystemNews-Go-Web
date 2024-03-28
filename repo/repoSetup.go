d0e231ae4e8dffc89659cd17a7395ae2fab070f9
cwdatlas <ascott@carroll.edu>
2024-03-27 20:11:12 -0600

    create and load database

---
+++ repo/repoSetup.go
@@ -0,0 +1,68 @@
+package repo
+
+import (
+	"SpaceNewsWeb/models"
+	"bufio"
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"os"
+	"strings"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	dsn := "host=localhost user=postgres password=12345 dbname=postgres port=5432 sslmode=disable TimeZone=UTC"
+	fmt.Print(dsn)
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+	err = database.AutoMigrate(&models.Article{})
+	if err != nil {
+		return
+	}
+	DB = database
+}
+
+func InitRepoData() {
+	// Read stored files and up
+	article := models.Article{}
+	empty := DB.Take(&article)
+	if empty.RowsAffected == 0 {
+		news := [2]string{"news/localNews.txt", "news/systemNews.txt"}
+		for _, i := range news {
+			dat, err := os.ReadFile(i)
+			check(err)
+			scanner := bufio.NewScanner(strings.NewReader(string(dat)))
+			scanner.Split(bufio.ScanLines)
+			line := 0
+			newArticle := models.Article{}
+			for scanner.Scan() {
+				text := scanner.Text()
+				fmt.Println(text)
+				if text == "<->" {
+					line = 0
+					DB.Create(&newArticle)
+					newArticle = models.Article{}
+				} else if line == 0 {
+					newArticle.Title = scanner.Text()
+					line++
+				} else if line == 1 {
+					newArticle.Author = scanner.Text()
+					line++
+				} else if line > 1 {
+					newArticle.Body += scanner.Text()
+					line++
+				}
+			}
+		}
+	}
+}
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
