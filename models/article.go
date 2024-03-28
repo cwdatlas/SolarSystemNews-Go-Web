a1df57752c7c4007621b3c99fca7c18297faaa00
cwdatlas <ascott@carroll.edu>
2024-03-27 20:10:34 -0600

    added body to article models

--- models/article.go
+++ models/article.go
@@ -8,6 +8,7 @@ type Article struct {
 	ID        uint   `json:"id" gorm:"primary_key"`
 	Title     string `json:"Title"`
 	Author    string `json:"author"`
+	Body      string `json:"body"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
@@ -15,9 +16,11 @@ type Article struct {
 type CreateArticleInput struct {
 	Title  string `json:"title" binding:"required" gorm:"size:5"`
 	Author string `json:"author" binding:"required" gorm:"size:5"`
+	Body   string `json:"body" binding:"required" gorm:"size:500"`
 }
 
 type UpdateArticleInput struct {
 	Title  string `json:"title" gorm:"size:40"`
 	Author string `json:"author" gorm:"size:40"`
+	Body   string `json:"body" gorm:"size:500"`
 }
