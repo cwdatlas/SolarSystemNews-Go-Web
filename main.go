3085f5624906f52531abfca40b48ea9312cd507b
cwdatlas <ascott@carroll.edu>
2024-04-01 15:34:50 -0600

    Added comments and added clean index endpoint

--- main.go
+++ main.go
@@ -7,23 +7,37 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+/*
+ * Author: Aidan Scott
+ * Main routs all traffic to their respective functions
+ */
 func main() {
+	// Gets default router
 	r := gin.Default()
 
+	// Connect to database
 	repo.ConnectDatabase()
+	// Init data in database, go to repo to learn more
 	repo.InitRepoData()
 
-	r.GET("/articles", controllers.FindArticles)
-	r.POST("/article", controllers.CreateArticle)
-	r.GET("/articles/:id", controllers.FindArticle)
+	//Rest endpoints
+	// Provides CRUD methods
+	r.GET("articles", controllers.FindArticles)
+	r.POST("article", controllers.CreateArticle)
+	r.GET("articles/:id", controllers.FindArticle)
 	r.PATCH("articles/:id", controllers.UpdateArticle)
 	r.DELETE("articles/:id", controllers.DeleteArticle)
 
-	//html
+	// Loads up templates to view
 	r.LoadHTMLGlob("templates/*")
-	r.GET("/", controllers.Index)
-	r.GET("/create_article", controllers.FormArticle)
-	r.POST("/create_article", controllers.ConsumeArtForm)
+
+	//Form and HTML endpoints
+	r.GET("/", controllers.Index)                         // Provides no param endpoint for index
+	r.GET("/:article_title", controllers.Index)           // provides search mechanism for index
+	r.GET("/create_article", controllers.FormArticle)     // Page that provides form
+	r.POST("/create_article", controllers.ConsumeArtForm) // Endpoint to post form to
+
+	// If error then print error and shutdown
 	err := r.Run()
 	if err != nil {
 		fmt.Println("error occurred:", err)
