4a239abac7b62a6594dc7b9159ca370967b61dad
cwdatlas <ascott@carroll.edu>
2024-04-01 18:36:02 -0600

    Added repo and validation services

---
+++ services/repo.go
@@ -0,0 +1,6 @@
+package services
+
+/*
+ * Author Aidan Scott
+ * this file is for accessing the repo
+ */
---
+++ services/validation.go
@@ -0,0 +1,6 @@
+package services
+
+/*
+ * Author Aidan Scott
+ * This file is for centralized validation across endpoints, rest and HTML
+ */
