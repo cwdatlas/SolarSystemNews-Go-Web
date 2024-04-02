efc45bcb7a3564e83ea388bf6841db105495b4d6
cwdatlas <ascott@carroll.edu>
2024-04-01 19:27:48 -0600

    Created repo service for central repo access

--- services/repo.go
+++ services/repo.go
@@ -1,6 +1,53 @@
 package services
 
+import (
+	"SpaceNewsWeb/models"
+	"SpaceNewsWeb/repo"
+	"fmt"
+	"time"
+)
+
 /*
  * Author Aidan Scott
  * this file is for accessing the repo
+ * Includes all CRUD operations
  */
+
+type dbError struct {
+	When time.Time
+	What string
+}
+
+func (e *dbError) Error() string {
+	return fmt.Sprintf("at %v, %s",
+		e.When, e.What)
+}
+func Create(article *models.Article) {
+	repo.DB.Create(article)
+}
+
+func UpdateByUArt(article *models.Article, update models.UpdateArticleInput) {
+	repo.DB.Model(article).Updates(update)
+}
+func Update(article *models.Article, update models.UpdateArticleInput) {
+	repo.DB.Model(article).Updates(update)
+}
+
+func DeleteA(column string, value string, article *models.Article) error {
+	if err := GetBy(column, value, article); err != nil {
+		return err
+	}
+	repo.DB.Delete(&article)
+	return nil
+}
+
+func GetBy(column string, value string, article *models.Article) error {
+	if err := repo.DB.Where(column+" = ?", value).First(article).Error; err != nil {
+		return &dbError{time.Now(), "Could not find Article"}
+	}
+	return nil
+}
+
+func GetMany(article *[]models.Article) {
+	repo.DB.Find(article)
+}
